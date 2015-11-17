a94e38e4a9320878016208127b1b10e038a5c8ac
Anuchit <anoochit@gmail.com>
2015-11-17 10:49:35 +0700

    fixed load layout at onCreate

--- app/src/main/java/net/redlinesoft/app/bleapplication/MainActivity.java
+++ app/src/main/java/net/redlinesoft/app/bleapplication/MainActivity.java
@@ -1,125 +1,73 @@
 package net.redlinesoft.app.bleapplication;
 
 
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.RemoteException;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
-import org.altbeacon.beacon.startup.BootstrapNotifier;
 import org.altbeacon.beacon.startup.RegionBootstrap;
 
-public class MainActivity extends AppCompatActivity  implements BootstrapNotifier {
+import java.text.DecimalFormat;
+import java.util.Collection;
+
+public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     protected static final String TAG = "RangingActivity";
-    private RegionBootstrap regionBootstrap;
+    BeaconManager beaconManager;
+    TextView txtRange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
-        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
-
-        // wake up the app when any beacon is seen (you can specify specific id filers in the parameters below)
-        Region region = new Region("net.redlinesoft.app.bleapplication", null, null, null);
-        regionBootstrap = new RegionBootstrap(this, region);
-
-    }
-
-/*    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new BackgroundPowerSaver(this);
+        txtRange= (TextView) findViewById(R.id.txtRange);
         beaconManager = BeaconManager.getInstanceForApplication(this);
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
-        // set the duration of the scan to be 1.1 seconds
-        beaconManager.setBackgroundScanPeriod(1100l);
-        // set the time between each scan to be 1 hour (3600 seconds)
-        beaconManager.setBackgroundBetweenScanPeriod(3600000l);
         beaconManager.bind(this);
-    }*/
+    }
 
-    /*@Override
+    @Override
     public void onBeaconServiceConnect() {
         beaconManager.setRangeNotifier(new RangeNotifier() {
-
-*//*            @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                for (Beacon beacon: beacons) {
-                    if (beacon.getDistance() < 5.0) {
-                        Log.d(TAG, "I see a beacon that is less than 5 meters away.");
-                        // Perform distance-specific action here
-                    }
-                }
-            }*//*
-
             @Override
-            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                for (Beacon beacon: beacons) {
-                    if (beacon.getDistance() < 2.0) {
-                        Log.d(TAG, "I see a beacon that is less than " + beacon.getDistance() + " meters away.");
-
-//                        Intent intent = new Intent(getApplicationContext(), Setting.class);
-//                        startActivity(intent);
-
-                        // Test Notification
-                        NotificationCompat.Builder mBuilder =
-                                new NotificationCompat.Builder(getApplicationContext())
-                                        .setSmallIcon(R.drawable.ic_notification)
-                                        .setContentTitle("iBeacon")
-                                        .setContentText("I see a beacon that is less than " + beacon.getDistance()+ " meters away.");
-                        NotificationManager mNotificationManager =
-                                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-                        mNotificationManager.notify(0, mBuilder.build());
-                    }
+            public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+                for (Beacon beacon : collection) {
+                    DecimalFormat df = new DecimalFormat("###.##");
+                    logToDisplay(df.format(beacon.getDistance()));
+                    Log.d(TAG, df.format(beacon.getDistance()));
                 }
-//                if (beacons.size() > 0) {
-//                    Log.i(TAG, "The first beacon I see is about " + beacons.iterator().next().getDistance() + " meters away.");
-//                }
             }
         });
 
         try {
             beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
-        } catch (RemoteException e) {    }
-    }*/
-
-
-    @Override
-    public void didEnterRegion(Region region) {
-        Log.d(TAG, "Got a didEnterRegion call");
-        regionBootstrap.disable();
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        this.startActivity(intent);
-
-
-        // Test Notification
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(getApplicationContext())
-                        .setSmallIcon(R.drawable.ic_notification)
-                        .setContentTitle("iBeacon")
-                        .setContentText("Enter iBeacon region");
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(0, mBuilder.build());
-
+        } catch (RemoteException e) {
+        }
     }
 
-    @Override
-    public void didExitRegion(Region region) {
-        // exit region
+    private void logToDisplay(final String format) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                txtRange.setText(format);
+            }
+        });
     }
 
-    @Override
-    public void didDetermineStateForRegion(int i, Region region) {
-        Log.d(TAG, "Got a state call " + i);
-    }
+
 }
